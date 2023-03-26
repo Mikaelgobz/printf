60021468d538e2050f5153fdab77249ce1b54caa
Sharl4 <sharlenepillay14@gmail.com>
2023-03-26 10:18:45 -0700

    team project

--- utils.c
+++ utils.c
@@ -5,13 +5,12 @@
  * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
-*/
-
+ */
 int is_printable(char c)
-
 {
-if (c >= 32 && c < 127)
-	return (1);
+	if (c >= 32 && c < 127)
+		return (1);
+
 	return (0);
 }
 
@@ -21,20 +20,20 @@ if (c >= 32 && c < 127)
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
-*/
-
+ */
 int append_hexa_code(char ascii_code, char buffer[], int i)
-
 {
-char map_to[] = "0123456789ABCDEF";
-/* The hexa format code is always 2 digits long */
-
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
-	ascii_code *= -1;
+		ascii_code *= -1;
+
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
+
 	buffer[i++] = map_to[ascii_code / 16];
 	buffer[i] = map_to[ascii_code % 16];
+
 	return (3);
 }
 
@@ -43,13 +42,12 @@ char map_to[] = "0123456789ABCDEF";
  * @c: Char to be evaluated
  *
  * Return: 1 if c is a digit, 0 otherwise
-*/
-
+ */
 int is_digit(char c)
-
 {
 	if (c >= '0' && c <= '9')
-	return (1);
+		return (1);
+
 	return (0);
 }
 
@@ -59,16 +57,14 @@ int is_digit(char c)
  * @size: Number indicating the type to be casted.
  *
  * Return: Casted value of num
-*/
-
+ */
 long int convert_size_number(long int num, int size)
-
 {
 	if (size == S_LONG)
-	return (num);
-
+		return (num);
 	else if (size == S_SHORT)
-	return ((short)num);
+		return ((short)num);
+
 	return ((int)num);
 }
 
@@ -78,14 +74,13 @@ long int convert_size_number(long int num, int size)
  * @size: Number indicating the type to be casted
  *
  * Return: Casted value of num
-*/
-
+ */
 long int convert_size_unsgnd(unsigned long int num, int size)
-
 {
 	if (size == S_LONG)
-	return (num);
+		return (num);
 	else if (size == S_SHORT)
-	return ((unsigned short)num);
+		return ((unsigned short)num);
+
 	return ((unsigned int)num);
 }
